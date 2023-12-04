7890fc5169fa3daa659c69f885ebc767ad284cf6
Emil410 <gasanovemil13@gmail.com>
2023-12-04 11:06:31 +0400

    end_project

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,7 @@
-var alphabet = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m",];
+var alphabet = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m",1,2,3,4,5,6,7,8,9,0];
 var RandomLetters = [];
-for (var i = 0; i < 4; i++) {
-    var randomIndex = Math.floor(Math.random() * 26);
+for (var i = 0; i < 8; i++) {
+    var randomIndex = Math.floor(Math.random() * 36);
     RandomLetters = RandomLetters + alphabet[randomIndex];
-}
-var randomNumber = Math.floor(1000 + Math.random() * 9999);
-console.log(RandomLetters + randomNumber);
+};
+console.log(RandomLetters)
